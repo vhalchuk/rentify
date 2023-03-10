c9160b4cdfae198e85886e92f550b6b6bfa438a7
Volodymyr Halchuk <volodymyr.halchuk.dev@gmail.com>
2023-03-10 21:35:00 +0400

    🎨 Add prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,23 @@
-/** @type {import("prettier").Config} */
+/**
+ * @typedef {import('prettier').Options & {
+ *   importOrder: string[]
+ * }} ExtendedConfig
+ */
+
+/** @type {ExtendedConfig} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  semi: false,
+  singleQuote: true,
+  importOrder: [
+    '<THIRD_PARTY_MODULES>',
+    '^~/app/(.*)$',
+    '^~/widgets/(.*)$',
+    '^~/features/(.*)$',
+    '^~/entities/(.*)$',
+    '^~/shared/(.*)$',
+    '^[./]'
+  ]
 };
 
 module.exports = config;
